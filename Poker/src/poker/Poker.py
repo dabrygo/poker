9c497c35f4d788b4f7ec067152c73aaacabcc334
Daniel <dbgodfrey89@msn.com>
2016-06-02 22:49:28 -0600

    Can compare hands that have two pairs.

--- Poker/src/poker/Poker.py
+++ Poker/src/poker/Poker.py
@@ -4,9 +4,10 @@ Created on Jun 2, 2016
 @author: Daniel
 '''
 import unittest
+from itertools import starmap
 
 class Card:
-    """A playing card that has rank and suit"""
+    """A playing card that has rank and suit."""
     ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 
              'T', 'J', 'Q', 'K', 'A']
     def __init__(self, string):
@@ -15,6 +16,9 @@ class Card:
     
     def __lt__(self, other):
         return Card.ranks.index(self.rank) < Card.ranks.index(other.rank)
+    
+    def __str__(self):
+        return self.rank + self.suit
         
         
 class CardTest(unittest.TestCase):
@@ -46,25 +50,46 @@ class CardTest(unittest.TestCase):
 
 
 class Hand:
-    """An unordered collection of five cards"""
+    """An unordered collection of cards."""
     def __init__(self, card_strings):
         self.cards = [Card(card) for card in card_strings]
         self.ranks = [card.rank for card in self.cards]
-        
-    def pair(self):
-        pairs = [card for card in self.cards if self.ranks.count(card.rank) == 2]
-        return pairs[0] if len(pairs) else False
     
+    def has_two(self, card):
+        return self.ranks.count(card.rank) == 2
+    
+    def find_pair(self):
+        return set([card.rank for card in self.cards if self.has_two(card)]) 
+    
+    def pair(self):
+        return str(self.find_pair()) if len(self.find_pair()) == 1 else []
+    
+    def two_pair(self):
+        return len(self.find_pair()) == 2
+        
     def sort_hand(self):
         self.cards = sorted(self.cards, reverse=True)
         
     def score(self):
+        if self.two_pair():
+            return 2
         if self.pair():
             return 1
         return 0
     
+    def __str__(self):
+        return ' '.join(str(card) for card in self.cards)
+    
 
 class HandTest(unittest.TestCase):
+    def test_is_pair(self):
+        pair = Hand(["5H", "5S"])
+        self.assertTrue(pair.has_two(Card("5H")))
+        
+    def test_is_not_pair(self):
+        pair = Hand(["4S", "5S"])
+        self.assertFalse(pair.has_two(Card("4S")))
+        
     def test_hand_has_pair(self):
         cards = ["5H", "5C", "6S", "7S", "KD"]
         hand = Hand(cards)
@@ -87,9 +112,15 @@ class HandTest(unittest.TestCase):
         
         self.assertLess(fives.pair(), eights.pair())
         
+    def test_can_see_two_pair(self):
+        one_pair = Hand(["2S", "2H", "3S", "4S"])
+        two_pair = Hand(["2S", "2H", "4S", "4H"])
+        self.assertTrue(two_pair.two_pair())
+        self.assertFalse(one_pair.two_pair())
+        
 
 class Game:
-    """A set of two hands, one of which is a winner"""
+    """A set of two hands, one of which is a winner."""
     def __init__(self, hand_1, hand_2):
         hand_1.sort_hand()
         self.hand_1 = hand_1
@@ -102,12 +133,20 @@ class Game:
     def both_players_have_pairs(self):
         return self.hand_1.score() == 1 == self.hand_2.score()
 
+    def both_players_have_two_pair(self):
+        return self.hand_1.score() == 2 == self.hand_2.score()
+
     def player_one_wins(self):
         if self.player_one_has_a_better_hand():
             return True
+        elif self.both_players_have_two_pair():
+            return self.hand_1.two_pair() > self.hand_2.two_pair() 
         elif self.both_players_have_pairs():
             return self.hand_1.pair() > self.hand_2.pair()
         return self.hand_1.cards[0] > self.hand_2.cards[0]
+
+    def player_two_wins(self):
+        return not self.player_one_wins()
     
 
 class GameTest(unittest.TestCase):
@@ -125,6 +164,6 @@ class GameTest(unittest.TestCase):
         
         game = Game(hand_1, hand_2)
         
-        self.assertFalse(game.player_one_wins())
+        self.assertTrue(game.player_two_wins())
         
     
