c133ec6c7f7ae407a3493b8eced0af17bc81b801
Daniel <dbgodfrey89@msn.com>
2016-06-02 22:04:37 -0600

    Can compare hands that have pairs.

--- Poker/src/poker/Poker.py
+++ Poker/src/poker/Poker.py
@@ -8,7 +8,7 @@ import unittest
 class Card:
     """A playing card that has rank and suit"""
     ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 
-             '10', 'J', 'Q', 'K', 'A']
+             'T', 'J', 'Q', 'K', 'A']
     def __init__(self, string):
         self.rank = string[0]
         self.suit = string[1]
@@ -37,7 +37,12 @@ class CardTest(unittest.TestCase):
     def test_ace_beats_queen(self):
         queen = Card("QH")
         ace = Card("AH")
-        self.assertTrue(ace > queen)
+        self.assertLess(queen, ace)
+        
+    def test_ten_beats_nine(self):
+        ten = Card("TH")
+        nine = Card("9H")
+        self.assertLess(nine, ten)
 
 
 class Hand:
@@ -46,23 +51,29 @@ class Hand:
         self.cards = [Card(card) for card in card_strings]
         self.ranks = [card.rank for card in self.cards]
         
-    def has_pair(self):
-        return any([self.ranks.count(card.rank) == 2 for card in self.cards])
+    def pair(self):
+        pairs = [card for card in self.cards if self.ranks.count(card.rank) == 2]
+        return pairs[0] if len(pairs) else False
     
     def sort_hand(self):
         self.cards = sorted(self.cards, reverse=True)
+        
+    def score(self):
+        if self.pair():
+            return 1
+        return 0
     
 
 class HandTest(unittest.TestCase):
     def test_hand_has_pair(self):
         cards = ["5H", "5C", "6S", "7S", "KD"]
         hand = Hand(cards)
-        self.assertTrue(hand.has_pair())
+        self.assertTrue(hand.pair())
         
     def test_hand_does_not_have_pair(self):
         cards = ["2H", "5C", "6S", "7S", "KD"]
         hand = Hand(cards)
-        self.assertFalse(hand.has_pair())
+        self.assertFalse(hand.pair())
         
     def test_sort_hand(self):
         cards = ["2H", "5C", "6S", "7S", "KD"]
@@ -70,16 +81,32 @@ class HandTest(unittest.TestCase):
         hand.sort_hand()
         self.assertEqual("K", hand.cards[0].rank)
         
+    def test_pair_eight_beats_pair_five(self):
+        eights = Hand(["8S", "8D"])
+        fives = Hand(["5H", "5C"])
+        
+        self.assertLess(fives.pair(), eights.pair())
+        
 
 class Game:
     """A set of two hands, one of which is a winner"""
     def __init__(self, hand_1, hand_2):
         hand_1.sort_hand()
         self.hand_1 = hand_1
-        hand_2.sort_hand
+        hand_2.sort_hand()
         self.hand_2 = hand_2
-        
+    
+    def player_one_has_a_better_hand(self):
+        return self.hand_1.score() > self.hand_2.score()
+
+    def both_players_have_pairs(self):
+        return self.hand_1.score() == 1 == self.hand_2.score()
+
     def player_one_wins(self):
+        if self.player_one_has_a_better_hand():
+            return True
+        elif self.both_players_have_pairs():
+            return self.hand_1.pair() > self.hand_2.pair()
         return self.hand_1.cards[0] > self.hand_2.cards[0]
     
 
@@ -90,6 +117,14 @@ class GameTest(unittest.TestCase):
         
         game = Game(hand_1, hand_2)
         
-        self.assertTrue(game.player_one_wins())        
+        self.assertTrue(game.player_one_wins())
+    
+    def test_player_2_has_high_pair(self):
+        hand_1 = Hand(["5H", "5C", "6S", "7S", "KD"])
+        hand_2 = Hand(["2C", "3S", "8S", "8D", "TD"])
+        
+        game = Game(hand_1, hand_2)
+        
+        self.assertFalse(game.player_one_wins())
         
     
