3b5063403a4cdab5e143e45862b10646e67b510e
Daniel <dbgodfrey89@msn.com>
2016-06-03 20:45:23 -0600

    Separate Poker tests into their own file. Clean up import statements.

---
+++ Poker/tests/poker/TestPoker.py
@@ -0,0 +1,207 @@
+'''
+Created on Jun 3, 2016
+
+@author: dbgod
+'''
+
+import unittest
+
+from poker.Poker import Card, Hand, Game
+
+class TestCard(unittest.TestCase):
+    def test_card(self):
+        card = Card("AS")
+        self.assertEqual("A", card.rank)
+        self.assertEqual("S", card.suit)
+    
+    def test_ace_high(self):
+        low_card = Card("2S")
+        high_card = Card("AS")
+        self.assertLess(low_card, high_card)
+        
+    def test_draw(self):
+        ace_1 = Card("AS")
+        ace_2 = Card("AH")
+        self.assertEqual(ace_1, ace_2)
+        
+    def test_ace_beats_queen(self):
+        queen = Card("QH")
+        ace = Card("AH")
+        self.assertLess(queen, ace)
+        
+    def test_ten_beats_nine(self):
+        ten = Card("TH")
+        nine = Card("9H")
+        self.assertLess(nine, ten)
+        
+
+class TestHand(unittest.TestCase):
+    def test_sort_hand(self):
+        hand = Hand(["5H", "5C", "6S", "7S", "KD"])
+        hand.sort_hand()
+        self.assertEqual("K", hand.cards[0].rank)
+    
+    # TODO No Magic numbers when it comes to testing score
+    def test_royal_flush(self):
+        hand = Hand(["JH", "KH", "TH", "AH", "QH"])
+        self.assertEqual(0, hand.score())
+      
+    def test_straight_flush(self):
+        hand = Hand(["JH", "8H", "TH", "9H", "7H"])
+        self.assertEqual(1, hand.score())
+        
+    def test_four_of_a_kind(self):
+        hand = Hand(["JH", "7H", "7D", "7C", "7S"])
+        self.assertEqual(2, hand.score())
+
+    def test_full_house(self):
+        hand = Hand(["5H", "5S", "7D", "7C", "7S"])
+        self.assertEqual(3, hand.score())
+
+    def test_flush(self):
+        hand = Hand(["TH", "7H", "2H", "KH", "3H"])
+        self.assertEqual(4, hand.score())
+        
+    def test_straight(self):
+        hand = Hand(["6H", "3S", "4H", "2C", "5H"])
+        self.assertEqual(5, hand.score())
+        
+    def test_three_of_a_kind(self):
+        hand = Hand(["6C", "KD", "3H", "3S", "3D"])
+        self.assertEqual(6, hand.score())
+        
+    def test_two_pair(self):
+        hand = Hand(["6C", "KD", "6H", "3S", "3D"])
+        self.assertEqual(7, hand.score())
+        
+    def test_pair(self):
+        hand = Hand(["6C", "KD", "6H", "7S", "3D"])
+        self.assertEqual(8, hand.score())
+        
+    def test_high_card(self):
+        hand = Hand(["6C", "KD", "9H", "7S", "3D"])
+        self.assertEqual(9, hand.score())
+
+
+
+class TestGame(unittest.TestCase):
+    def test_pair_eight_beats_pair_five(self):
+        eights = Hand(["8S", "8D"])
+        fives = Hand(["5H", "5C"])
+        
+        game = Game(eights, fives)
+        
+        self.assertTrue(game.player_one_wins())
+    
+    def test_player_1_has_high_card(self):
+        hand_1 = Hand(["5D", "8C", "9S", "JS", "AC"])
+        hand_2 = Hand(["2C", "5C", "7D", "8S", "QH"])
+        
+        game = Game(hand_1, hand_2)
+        
+        self.assertTrue(game.player_one_wins())
+    
+    def test_player_2_has_high_pair(self):
+        hand_1 = Hand(["5H", "5C", "6S", "7S", "KD"])
+        hand_2 = Hand(["2C", "3S", "8S", "8D", "TD"])
+        
+        game = Game(hand_1, hand_2)
+        
+        self.assertTrue(game.player_two_wins())
+        
+    def test_two_pair_beats_higher_pair(self):
+        hand_1 = Hand(["2S", "2H", "3S", "3H"])
+        hand_2 = Hand(["KS", "KH", "6S", "5H"])
+        
+        game = Game(hand_1, hand_2)
+        
+        self.assertTrue(game.player_one_wins())
+    
+    def test_pair_jacks_beats_pair_tens(self):
+        hand_1 = Hand(["TH", "8H", "5C", "QS", "TC"])
+        hand_2 = Hand(["9H", "4D", "JC", "KS", "JS"])
+         
+        game = Game(hand_1, hand_2)
+         
+        self.assertTrue(game.player_two_wins())
+
+    def test_three_of_a_kind_beats_two_pair(self):
+        hand_1 = Hand(["2S", "2D", "2H", "3S", "4D"])
+        hand_2 = Hand(["AS", "AD", "KH", "KS", "QD"])
+        
+        game = Game(hand_1, hand_2)
+        
+        self.assertTrue(game.player_one_wins)
+                      
+
+# TODO: More "tied" tests
+class TestTie(unittest.TestCase):
+    @unittest.skip("TODO: Draw conditions")
+    def test_both_players_have_royal_flush(self):
+        pass
+    
+    def test_one_player_has_smaller_straight_flush(self):
+        hand_1 = Hand(["2H", "3H", "4H", "5H", "6H"])
+        hand_2 = Hand(["3S", "4S", "5S", "6S", "7S"])
+        
+        game = Game(hand_1, hand_2)
+        
+        self.assertTrue(game.player_two_wins())
+        
+    def test_one_player_has_smaller_four_of_a_kind(self):
+        hand_1 = Hand(["5H", "5S", "5C", "5D", "QH"])
+        hand_2 = Hand(["KH", "KS", "KC", "KD", "2H"])
+        
+        game = Game(hand_1, hand_2)
+        
+        self.assertTrue(game.player_two_wins())
+        
+    @unittest.skip("Worry about multideck games later...")
+    def test_both_players_have_same_four_of_a_kind(self):
+        pass
+    
+    @unittest.skip("FIXME")
+    def test_one_player_has_smaller_triplet_in_full_house(self):
+        hand_1 = Hand(["2H", "2D", "4C", "4D", "4S"])
+        hand_2 = Hand(["3C", "3D", "3S", "9S", "9D"])
+         
+        game = Game(hand_1, hand_2)
+         
+        self.assertTrue(game.player_one_wins())
+    
+    @unittest.skip("Worry about multideck games later...")
+    def test_one_player_has_smaller_pair_in_full_house(self):
+        hand_1 = Hand(["4C", "4D", "4S", "2H", "2D"])
+        hand_2 = Hand(["4C", "4D", "4S", "9S", "9D"])
+         
+        game = Game(hand_1, hand_2)
+         
+        self.assertTrue(game.player_two_wins())
+    
+    @unittest.skip("TODO Multideck draw condition")
+    def test_players_have_same_full_house(self):
+        hand_1 = Hand(["4C", "4D", "4S", "2H", "2D"])
+        hand_2 = Hand(["4C", "4D", "4S", "2S", "2C"])
+        
+        game = Game(hand_1, hand_2)
+        
+        print(game)
+    
+    @unittest.skip("TEMPORARY SKIP")
+    def test_one_player_has_smaller_high_card_in_flush(self):
+        hand_1 = Hand(["2H", "4H", "5H", "6H", "7H"]) 
+        hand_2 = Hand(["2C", "4C", "5C", "6C", "8C"]) 
+        
+        game = Game(hand_1, hand_2)
+        
+        self.assertTrue(game.player_two_wins())
+    
+    @unittest.skip("TEMPORARY SKIP") 
+    def test_one_player_has_smallest_card_in_flush(self):
+        hand_1 = Hand(["9C", "JC", "QC", "KC", "AC"]) 
+        hand_2 = Hand(["8H", "JH", "QH", "KH", "AH"])
+        
+        game = Game(hand_1, hand_2)
+        
+        self.assertTrue(game.player_one_wins())
+  
--- Poker/tests/poker/TestWinPatterns.py
+++ Poker/tests/poker/TestWinPatterns.py
@@ -5,77 +5,77 @@ Created on Jun 3, 2016
 '''
 import unittest
 
-from poker import Poker 
-from poker import WinPatterns
+from poker.Poker import Hand 
+from poker.WinPatterns import HighCard, Pair, TwoPair, ThreeOfAKind, Straight, Flush, FullHouse, FourOfAKind, StraightFlush, RoyalFlush
 
 class TestHighCard(unittest.TestCase):
     def test_low_high_card(self):
-        bad_hand = Poker.Hand(["7D", "2H", "3D", "5C", "4S"])
-        self.assertEqual("7", WinPatterns.HighCard(bad_hand).values()[0])
+        bad_hand = Hand(["7D", "2H", "3D", "5C", "4S"])
+        self.assertEqual("7", HighCard(bad_hand).values()[0])
     
     def test_ace_high(self):
-        good_hand = Poker.Hand(["AD", "KD", "QD", "JD", "TD"])
-        self.assertEqual("A", WinPatterns.HighCard(good_hand).values()[0])
+        good_hand = Hand(["AD", "KD", "QD", "JD", "TD"])
+        self.assertEqual("A", HighCard(good_hand).values()[0])
 
 
 class TestPair(unittest.TestCase):    
     def test_is_pair(self):
-        pair = Poker.Hand(["5H", "5S"])
-        self.assertTrue(WinPatterns.Pair(pair).criterion())
+        pair = Hand(["5H", "5S"])
+        self.assertTrue(Pair(pair).criterion())
                 
     def test_is_not_pair(self):
-        pair = Poker.Hand(["4S", "5S"])
-        self.assertFalse(WinPatterns.Pair(pair).criterion())
+        pair = Hand(["4S", "5S"])
+        self.assertFalse(Pair(pair).criterion())
         
     def test_know_rank_of_pair(self):
-        hand = Poker.Hand(["7S", "2H", "3D", "7C", "KD"])
-        self.assertEqual(["7"], WinPatterns.Pair(hand).values())
+        hand = Hand(["7S", "2H", "3D", "7C", "KD"])
+        self.assertEqual(["7"], Pair(hand).values())
 
 
 class TestTwoPair(unittest.TestCase):    
     def test_is_two_pair(self):
-        two_pair = Poker.Hand(["5H", "5S", "8H", "8D"])
-        self.assertTrue(WinPatterns.TwoPair(two_pair).criterion())
+        two_pair = Hand(["5H", "5S", "8H", "8D"])
+        self.assertTrue(TwoPair(two_pair).criterion())
                 
     def test_lone_pair_is_not_two_pair(self):
-        pair = Poker.Hand(["4S", "4S", "7H", "8D"])
-        self.assertFalse(WinPatterns.TwoPair(pair).criterion())
+        pair = Hand(["4S", "4S", "7H", "8D"])
+        self.assertFalse(TwoPair(pair).criterion())
         
     def test_know_ranks_of_two_pair(self):
-        hand = Poker.Hand(["7S", "3H", "3D", "7C", "KD"])
-        self.assertEqual(["3", "7"], WinPatterns.TwoPair(hand).values())
+        hand = Hand(["7S", "3H", "3D", "7C", "KD"])
+        self.assertEqual(["3", "7"], TwoPair(hand).values())
 
 
 class TestThreeOfAKind(unittest.TestCase):    
     def test_is_three_of_a_kind(self):
-        pair = Poker.Hand(["5H", "5S", "5D"])
-        self.assertTrue(WinPatterns.ThreeOfAKind(pair).criterion())
+        pair = Hand(["5H", "5S", "5D"])
+        self.assertTrue(ThreeOfAKind(pair).criterion())
                 
     def test_pair_is_not_three_of_a_kind(self):
-        pair = Poker.Hand(["4S", "5S", "5D"])
-        self.assertFalse(WinPatterns.ThreeOfAKind(pair).criterion())
+        pair = Hand(["4S", "5S", "5D"])
+        self.assertFalse(ThreeOfAKind(pair).criterion())
         
     def test_four_of_a_kind_is_not_three_of_a_kind(self):
-        hand = Poker.Hand(["7S", "7H", "3D", "7C", "7D"])
-        self.assertFalse(WinPatterns.ThreeOfAKind(hand).criterion())
+        hand = Hand(["7S", "7H", "3D", "7C", "7D"])
+        self.assertFalse(ThreeOfAKind(hand).criterion())
         
     def test_know_rank_of_three_of_a_kind(self):
-        hand = Poker.Hand(["7S", "7H", "3D", "7C", "KD"])
-        self.assertEqual(["7"], WinPatterns.ThreeOfAKind(hand).values())
+        hand = Hand(["7S", "7H", "3D", "7C", "KD"])
+        self.assertEqual(["7"], ThreeOfAKind(hand).values())
         
 
 class TestStraight(unittest.TestCase):
     def test_small_straight(self):
-        small_straight = Poker.Hand(["2S", "3D", "4C", "5H", "6C"])
-        self.assertTrue(WinPatterns.Straight(small_straight).criterion())
+        small_straight = Hand(["2S", "3D", "4C", "5H", "6C"])
+        self.assertTrue(Straight(small_straight).criterion())
     
     def test_is_not_straight(self):
-        straight = Poker.Hand(["8S", "3D", "4C", "5H", "6C"])
-        self.assertFalse(WinPatterns.Straight(straight).criterion())
+        straight = Hand(["8S", "3D", "4C", "5H", "6C"])
+        self.assertFalse(Straight(straight).criterion())
         
     def test_big_straight(self):
-        big_straight = Poker.Hand(["AS", "KD", "QH", "JS", "TC"])
-        self.assertTrue(WinPatterns.Straight(big_straight).criterion())
+        big_straight = Hand(["AS", "KD", "QH", "JS", "TC"])
+        self.assertTrue(Straight(big_straight).criterion())
         
     def test_values(self):
         pass
@@ -83,78 +83,78 @@ class TestStraight(unittest.TestCase):
 
 class TestFlush(unittest.TestCase):
     def test_is_a_flush(self):
-        flush = Poker.Hand(["5D", "6D", "2D", "KD", "TD"])
-        self.assertTrue(WinPatterns.Flush(flush).criterion())
+        flush = Hand(["5D", "6D", "2D", "KD", "TD"])
+        self.assertTrue(Flush(flush).criterion())
     
     def test_one_not_same_suit(self):
-        not_flush = Poker.Hand(["5D", "6D", "2H", "KD", "TD"])
-        self.assertFalse(WinPatterns.Flush(not_flush).criterion())
+        not_flush = Hand(["5D", "6D", "2H", "KD", "TD"])
+        self.assertFalse(Flush(not_flush).criterion())
         
 
 class TestFullHouse(unittest.TestCase):
     def test_is_full_house(self):
-        full_house = Poker.Hand(["4S", "4D", "7H", "7C", "7D"])
-        self.assertTrue(WinPatterns.FullHouse(full_house).criterion())
+        full_house = Hand(["4S", "4D", "7H", "7C", "7D"])
+        self.assertTrue(FullHouse(full_house).criterion())
         
     def test_two_pair_is_not_full_house(self):
-        two_pair = Poker.Hand(["4S", "4D", "7H", "7C", "8D"])
-        self.assertFalse(WinPatterns.FullHouse(two_pair).criterion())
+        two_pair = Hand(["4S", "4D", "7H", "7C", "8D"])
+        self.assertFalse(FullHouse(two_pair).criterion())
         
     def test_suits_do_not_make_a_full_house(self):
-        suit_house = Poker.Hand(["2S", "4S", "7H", "6H", "8H"])
-        self.assertFalse(WinPatterns.FullHouse(suit_house).criterion())
+        suit_house = Hand(["2S", "4S", "7H", "6H", "8H"])
+        self.assertFalse(FullHouse(suit_house).criterion())
         
     def test_value_full_house(self):
-        hand = Poker.Hand(["4C", "2H", "2D", "4D", "4S"])
-        self.assertEqual(["4", "2"], WinPatterns.FullHouse(hand).values())
+        hand = Hand(["4C", "2H", "2D", "4D", "4S"])
+        self.assertEqual(["4", "2"], FullHouse(hand).values())
         
 
 class TestFourOfAKind(unittest.TestCase):    
     def test_is_four_of_a_kind(self):
-        pair = Poker.Hand(["5H", "5S", "5D", "5C"])
-        self.assertTrue(WinPatterns.FourOfAKind(pair).criterion())
+        pair = Hand(["5H", "5S", "5D", "5C"])
+        self.assertTrue(FourOfAKind(pair).criterion())
                 
     def test_pair_is_not_four_of_a_kind(self):
-        pair = Poker.Hand(["4S", "5S", "5D"])
-        self.assertFalse(WinPatterns.FourOfAKind(pair).criterion())
+        pair = Hand(["4S", "5S", "5D"])
+        self.assertFalse(FourOfAKind(pair).criterion())
          
     def test_three_of_a_kind_is_not_four_of_a_kind(self):
-        hand = Poker.Hand(["7S", "7H", "3D", "2C", "7D"])
-        self.assertFalse(WinPatterns.FourOfAKind(hand).criterion())
+        hand = Hand(["7S", "7H", "3D", "2C", "7D"])
+        self.assertFalse(FourOfAKind(hand).criterion())
          
     def test_know_rank_of_four_of_a_kind(self):
-        hand = Poker.Hand(["7S", "7H", "3D", "7C", "7D"])
-        self.assertEqual(["7"], WinPatterns.FourOfAKind(hand).values())
+        hand = Hand(["7S", "7H", "3D", "7C", "7D"])
+        self.assertEqual(["7"], FourOfAKind(hand).values())
  
 
 class TestStraightFlush(unittest.TestCase):
     def test_is_straight_flush(self):
-        hand = Poker.Hand(["2S", "3S", "4S", "5S", "6S"])
-        self.assertTrue(WinPatterns.StraightFlush(hand).criterion())
+        hand = Hand(["2S", "3S", "4S", "5S", "6S"])
+        self.assertTrue(StraightFlush(hand).criterion())
         
     def test_straight_is_not_always_straight_flush(self):
-        hand = Poker.Hand(["2S", "3D", "4S", "5S", "6S"])
-        self.assertFalse(WinPatterns.StraightFlush(hand).criterion())
+        hand = Hand(["2S", "3D", "4S", "5S", "6S"])
+        self.assertFalse(StraightFlush(hand).criterion())
         
     def test_flush_is_not_always_straight_flush(self):
-        hand = Poker.Hand(["2S", "3S", "4S", "5S", "7S"])
-        self.assertFalse(WinPatterns.StraightFlush(hand).criterion())
+        hand = Hand(["2S", "3S", "4S", "5S", "7S"])
+        self.assertFalse(StraightFlush(hand).criterion())
         
     def test_straight_flush_values(self):
-        hand = Poker.Hand(["7H", "5H", "8H", "4H", "6H"])
-        self.assertEqual(["4", "5", "6", "7", "8"], WinPatterns.StraightFlush(hand).values())
+        hand = Hand(["7H", "5H", "8H", "4H", "6H"])
+        self.assertEqual(["4", "5", "6", "7", "8"], StraightFlush(hand).values())
   
      
 class TestRoyalFlush(unittest.TestCase):
     def test_royal_flush(self):
-        hand = Poker.Hand(["AD", "KD", "QD", "JD", "TD"])
-        self.assertTrue(WinPatterns.RoyalFlush(hand).criterion())
+        hand = Hand(["AD", "KD", "QD", "JD", "TD"])
+        self.assertTrue(RoyalFlush(hand).criterion())
     
     def test_straight_flush_but_not_royal(self):
-        hand = Poker.Hand(["KD", "QD", "JD", "TD", "9D"])
-        self.assertFalse(WinPatterns.RoyalFlush(hand).criterion())
+        hand = Hand(["KD", "QD", "JD", "TD", "9D"])
+        self.assertFalse(RoyalFlush(hand).criterion())
     
     def test_straight_but_not_royal_flush(self):
-        hand = Poker.Hand(["AS", "KD", "QD", "JD", "TD"])
-        self.assertFalse(WinPatterns.RoyalFlush(hand).criterion())
+        hand = Hand(["AS", "KD", "QD", "JD", "TD"])
+        self.assertFalse(RoyalFlush(hand).criterion())
         
